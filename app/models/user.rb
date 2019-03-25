27da51ddd6cab59c8b2c83b78bdb0fbb3fc86f57
dg-narola <dg@narola.email>
2019-03-25 18:02:25 +0530

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 
-  def find_for_database_authentication(warden_conditions)
+  def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login == conditions.delete(:login)
       where(conditions.to_h).where(['lower(username) = :value OR lower(email) =
