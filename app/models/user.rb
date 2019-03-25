aa82dffb4de39928a12a7c2ca820fe0248738942
dg-narola <dg@narola.email>
2019-03-25 18:04:13 +0530

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,19 +12,18 @@ class User < ApplicationRecord
   attr_writer :login
 
   def login
-    @login || username || email
+    @login || self.username || self.email
   end
 
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
 
   def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login == conditions.delete(:login)
-      where(conditions.to_h).where(['lower(username) = :value OR lower(email) =
-                                      :value',
-                                    { value: login.downcase }]).first
-    elsif conditions.key?(:username) || conditions.key?(:email)
-      where(conditions.to_h).first
-    end
+        conditions = warden_conditions.dup
+        if login = conditions.delete(:login)
+          where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value",
+             { :value => login.downcase }]).first
+        elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+          where(conditions.to_h).first
+        end
   end
 end
