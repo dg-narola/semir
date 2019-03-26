a03a434a81b298108324d909bb8b2cbb9b139613
dg-narola <dg@narola.email>
2019-03-26 14:15:45 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_mailer.default_url_options =
-    { host: 'rspec-seminar.herokuapp.com', protocol: 'http' }
+    { host: 'rspec-semir.herokuapp.com', protocol: 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
