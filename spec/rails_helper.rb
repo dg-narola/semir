821421f3086c8502104377bf5c11c1b808d410b7
dg-narola <dg@narola.email>
2019-03-27 14:32:46 +0530

    changes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,34 +17,34 @@ require 'database_cleaner'
 require 'capybara'
 require 'capybara/rspec'
 require 'capybara/dsl'
-require 'selenium/webdriver'
+require 'selenium-webdriver'
 require 'shoulda/matchers'
 
 selenium_url = 'http://localhost:3002/wd/hub'
 
 # use :chrome instead of :selenium_remote
-# Capybara.register_driver :selenium_remote do |app|
-#   options = ::Selenium::WebDriver::Chrome.driver_path::Options.new
-#
-#   options.add_argument('--headless')
-#   options.add_argument('--no-sandbox')
-#   options.add_argument('--disable-dev-shm-usage')
-#   options.add_argument('--window-size=1400,1400')
-#
-#   Capybara::Selenium::Driver.new(app,
-#                                  url: selenium_url, browser: :chrome,
-#                                  options: options)
-# end
+Capybara.register_driver :selenium_remote do |app|
+  options = ::Selenium::WebDriver::Firefox.driver_path::Options.new
 
-Capybara.register_driver :selenium_headless do |app|
-Capybara::Selenium::Driver.load_selenium
-browser_options = ::Selenium::WebDriver::Firefox::Options.new
-browser_options.args << '-headless'
-Capybara::Selenium::Driver.new(app, browser: :firefox, options: browser_options)
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app,
+                                 url: selenium_url, browser: :firefox,
+                                 options: options)
 end
 
+# Capybara.register_driver :selenium_headless do |app|
+# Capybara::Selenium::Driver.load_selenium
+# browser_options = ::Selenium::WebDriver::Firefox::Options.new
+# browser_options.args << '-headless'
+# Capybara::Selenium::Driver.new(app, browser: :firefox, options: browser_options)
+# end
+
 Capybara.server = :webrick # puma
-Capybara.javascript_driver = :selenium_headless #:chrome
+Capybara.javascript_driver = :selenium_remote #:chrome
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
