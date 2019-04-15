c88482a5d6c0195f9694f818f29f78d870bee317
dg-narola <dg@narola.email>
2019-04-15 12:40:08 +0530

    changes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,10 +43,11 @@ end
 # Capybara::Selenium::Driver.new(app, browser: :firefox, options: browser_options)
 # end
 
+Capybara.app_host = "http://frontend:3000"
 Capybara.server = :webrick # puma
 Capybara.javascript_driver = :selenium_remote #:chrome
-Capybara.server_port = 5001 # We don't want it to collide with standard rails server on port 5000
-Capybara.server_host = "0.0.0.0" # Start server on localhost as meta-address
+# Capybara.server_port = 5001 # We don't want it to collide with standard rails server on port 5000
+# Capybara.server_host = "0.0.0.0" # Start server on localhost as meta-address
 # Capybara.server = :puma, { Silent: true } # Supress puma STDOUT in console
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
