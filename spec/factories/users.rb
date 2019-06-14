e42175c2a7a8d3cbb23366215908c849366ee38e
dg-narola <dg@narola.email>
2019-06-14 10:22:44 +0530

    changes

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,24 +5,10 @@ FactoryGirl.define do
     sequence(:email) do |n|
       "example#{n}@example.com"
     end
-    password 'password'
-    password_confirmation 'password'
-    username 'dharani'
-    # confirmed_at Time.zone.now
-    # current_sign_in_at Time.zone.now
+    password "password"
+    password_confirmation "password"
+    sequence(:username) do |n|
+      "dharani#{n}"
+    end
   end
-
-  # trait :confirmed do
-  #   confirmed_at Time.zone.now
-  #   current_sign_in_at Time.zone.now
-  # end
-
-  # trait :verified do
-  # end
-  #
-  # trait :unverified do
-  #   email nil
-  #   password nil
-  #   password_confirmation nil
-  # end
 end
